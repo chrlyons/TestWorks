b2baded0f4f75ace8d828ff60a1cfe0942009df4
Christian Lyons <christian.lyons@pm.me>
2024-05-13 18:47:54 -0500

    Move test.

--- backend/tests/integration/redis/test_redis.py
+++ backend/tests/integration/redis/test_redis.py
@@ -1,8 +1,10 @@
 import pytest
 import time
 import json
+from unittest.mock import patch
+from datetime import datetime, timezone, timedelta
 
-from app.crud import get_user_by_username, redis_client, update_redis_user_session
+from app.crud import check_user_token_expiration, get_user_by_username, redis_client, update_redis_user_session
 
 
 @pytest.mark.integration
@@ -31,3 +33,37 @@ class TestRedisOperations:
         # Check if the session information has been updated in Redis
         assert updated_session is not None
         assert json.loads(updated_session) == session_info
+
+    @pytest.fixture
+    def redis_data(self):
+        return {
+            "user:1": json.dumps(
+                {
+                    "token_expires": (
+                            datetime.now(timezone.utc) - timedelta(days=1)
+                    ).isoformat()
+                }
+            ),
+            "user:2": json.dumps(
+                {
+                    "token_expires": (
+                            datetime.now(timezone.utc) + timedelta(days=1)
+                    ).isoformat()
+                }
+            ),
+        }
+
+    def test_check_user_token_expiration(self, redis_data):
+        with patch("app.crud.redis_client.scan_iter", return_value=redis_data.keys()):
+            with patch(
+                    "app.crud.redis_client.get",
+                    side_effect=lambda k: redis_data[k].encode("utf-8"),
+            ):
+                with patch("app.crud.redis_client.ttl", return_value=3600):
+                    with patch("app.crud.remove_user_from_database") as mock_remove:
+                        check_user_token_expiration()
+                        # Assert that remove_user_from_database is called with each user key
+                        mock_remove.assert_any_call("user:1")
+                        mock_remove.assert_any_call("user:2")
+                        # Assert that remove_user_from_database is called exactly twice
+                        assert mock_remove.call_count == 2
